3319c829e06dd1f87854fbb38c3d1519b6289e27
NoobException <noob-exception@wp.pl>
2020-01-09 20:39:00 +0100

    Component Decomposition

---
+++ Headers/Game/GameController.hpp
@@ -0,0 +1,9 @@
+#ifndef GAME_GAMECONTROLLER_HPP
+#define GAME_GAMECONTROLLER_HPP
+
+class GameController
+{
+
+};
+
+#endif //  GAMECONTROLLER_HPP
---
+++ Headers/Game/GamePresenter.hpp
@@ -0,0 +1,9 @@
+#ifndef GAME_GAMEPRESENTER_HPP
+#define GAME_GAMEPRESENTER_HPP
+
+class GamePresenter
+{
+
+};
+
+#endif //  GAMECONTROLLER_HPP
